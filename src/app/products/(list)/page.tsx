33161725239bfa64479d57a2b93aa37756104163
Daniel Vitor <ripsea.rpg321@gmail.com>
2024-08-22 20:37:18 -0300

    Adding function to generate fake products

--- src/app/products/(list)/page.tsx
+++ src/app/products/(list)/page.tsx
@@ -1,33 +1,58 @@
 import ProductCard from "./components/product-card";
 
-function generateProducts(): Array<any> {
-  const arraySize = 100
-  const projects: Array<any> = []
-
-  for (let i = 0; i < arraySize; i++) {
-    projects.push({
-      name: `Product ${i}`,
-      image: new URL(
-        `https://picsum.photos/seed/${new Date().getTime() * (i + 1)}/320/640`,
-      ).toString(),
-    })
-  }
-
-  return projects
-}
-
 export default function Page() {
   return (
     <section className="min-w-[800px] min-h-[800px] flex flex-col gap-20 items-center justify-start">
       <h1>PetShow - Listing Products</h1>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-x-4 gap-y-4">
+      <div className="flex flex-col gap-4">
         {
-          generateProducts().map(({ image, name }, idx) => (
-            <ProductCard key={idx} name={name} url_image={image} />
+          generateProducts().map(({ image, name, variations }, idx) => (
+            <ProductCard 
+              key={idx} 
+              name={name} 
+              url_image={image}
+              description="Mussum Ipsum, cacilds vidis litro abertis. Quem num gosta di mim que vai caçá sua turmis! Bota 1 metro de cachacis aí pra"
+              variations={variations}
+            />
           ))
         }
       </div>
     </section>
   );
+}
+
+function generateProducts(): Array<any> {
+  const arraySize = 45;
+  const products: Array<any> = [];
+
+  for (let i = 0; i < arraySize; i++) {
+    products.push({
+      name: `Product ${i + 1}`,
+      image: new URL(
+        `https://picsum.photos/seed/${new Date().getTime() * (i + 1)}/320/640`,
+      ).toString(),
+      variations: [
+        {
+          name: "Tamanho P",
+          price: 19.99,
+          stock: 15,
+          inPromotion: true,
+          promotion: {
+            newPrice: 15.99,
+            startDate: "2024-08-01",
+            endDate: "2024-08-31"
+          }
+        },
+        {
+          name: "Tamanho M",
+          price: 21.99,
+          stock: 10,
+          inPromotion: false,
+        },
+      ],
+    });
+  }
+
+  return products;
 }
